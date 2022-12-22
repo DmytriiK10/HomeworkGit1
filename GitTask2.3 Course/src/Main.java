01a2562ef681399a0ab5a9c51a8126dfa9c54e32
DmytriiK10 <118010827+DmytriiK10@users.noreply.github.com>
2022-12-22 19:54:35 +0200

    Add files via upload

---
+++ GitTask2.3 Course/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        String cource = "1 курс історичного факультету";
+        System.out.println(cource);
+    }
+}
---
+++ GitTask2/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        String nameSurname = "Іванов Іван";
+        System.out.println(nameSurname);
+    }
+}
